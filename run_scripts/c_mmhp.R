e3cd7a7505913f0e29a5931cc2b92c192c4d32fe
Owen <owengward@gmail.com>
2020-06-13 12:44:28 -0400

    add submit script

--- run_scripts/c_mmhp.R
+++ run_scripts/c_mmhp.R
@@ -45,7 +45,7 @@ source('lib/residualStructureScore.R')
 
 
 
-
+#### load the data ####
 full_data <- readRDS("data/mice.RData")
 # A=c9, B=c10, C=c12, D=c15, E=c16, F=c17, G=c18, H=c37, I=c38. J=c45
 cohort_names <- paste("cohort",c(9,10,12,15,16,17,18,37,38,45),sep='')
@@ -80,12 +80,9 @@ print(paste("Cohort",current_cohort))
 #######
 #######
 #######
-## The stan model used here is important
-#######
-#######
-#######
 
 
+#### fit the stan model ####
 print(paste("Cohort",current_cohort))
 stan_input_lst <- prepareDataStan(current_cohort)
 stan_input_lst$alpha_id <- expert_rank_10[[current_cohort]][1]
@@ -102,7 +99,7 @@ save(sim_cohort_mmhp, fit_cohort_mmhp,
                   "/cohort_mmhp_stan_result_",cohort_names[current_cohort],
                   ".RData",sep=''))
 
-# Interpolate Latent States
+#### Interpolate Latent States ####
 
 print(current_cohort)
 load(paste(save_data_path,cohort_names[current_cohort],
@@ -199,7 +196,7 @@ save(state_array_list,initial_state_list,termination_state_list,
      file=paste(save_data_path,cohort_names[current_cohort],
                 "/cmmhp_est_zt_",cohort_names[current_cohort],".RData",sep=''))
 
-### Predictions for this model
+### Predictions for this model ####
 print(current_cohort)
 stan_train_input_lst <- prepareDataStanTrain(current_cohort)
 stan_train_input_lst$alpha_id <- expert_rank_10[[current_cohort]][1]
@@ -216,7 +213,7 @@ save(sim_cohort_mmhp, fit_cohort_mmhp,
                   "/cohort_mmhp_predict_stan_result_",cohort_names[current_cohort],
                   ".RData",sep=''))
 
-# then the pearson residuals for this fit will also be computed here
+#### pearson residuals for this fit ####
 mice_number <- 12
 
 load(paste(save_data_path,cohort_names[current_cohort],
@@ -289,7 +286,7 @@ for(i in 1:mice_number){
 
 
 
-# then save this mmhp_residual_matrix
+#### then save this mmhp_residual_matrix ####
 saveRDS(m3_residual_matrix,
         file = paste(save_data_path,cohort_names[current_cohort],
                      "/cmmhp_pr_matrix_",cohort_names[current_cohort],
---
+++ run_scripts/submit.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+#SBATCH -A tzsts
+#SBATCH -J cmmhp
+#SBATCH -c 8
+#SBATCH -t 720:00
+#SBATCH --mem-per-cpu 12gb
+##SBATCH -a 2-5
+
+module load R
+echo "Launching R"
+date
+
+R CMD BATCH --no-save --vanilla c_mmhp.R routput_cmmhp
+echo "Completed"
+date
+
+#end of script
