e50d72d68eaeaca774c78eeadc94a21c3ab3f588
Owen <owengward@gmail.com>
2020-07-09 17:34:59 -0400

    submit

--- run_scripts/submit.sh
+++ run_scripts/submit.sh
@@ -1,17 +1,19 @@
 #!/bin/sh
 #
 #SBATCH -A tzsts
-#SBATCH -J immhp
+#SBATCH -J cmmhp
 #SBATCH -c 8
-#SBATCH -t 720:00
+#SBATCH -t 1500:00
 #SBATCH --mem-per-cpu 12gb
 #SBATCH -a 1-10
+#SBATCH --mail-user=ogw2103@columbia.edu
+#SBATCH --mail-type=ALL
 
 module load R
 echo "Launching R"
 date
 
-R CMD BATCH --no-save --vanilla i_mmhp.R routput_immhp
+R CMD BATCH --no-save --vanilla c_mmhp.R routput_cmmhp_dc
 echo "Completed"
 date
 
