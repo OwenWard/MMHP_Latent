c7e35ce46e5ace0718d3ffbe6fa7d299fe12d9db
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2020-07-15 15:51:59 -0400

    update these to reflect renamed stan model

--- run_scripts/c_mmhp_mod.R
+++ run_scripts/c_mmhp_mod.R
@@ -92,7 +92,7 @@ print(paste("Cohort",current_cohort))
 # stan_input_lst$alpha_id <- expert_rank_10[[current_cohort]][1]
 # stan_input_lst$delta_1 <- rep(0.5,stan_input_lst$N_til)
 # 
-# fit_cohort_mmhp <- stan("lib/model3_1_bound.stan",  ## this will need to be updated
+# fit_cohort_mmhp <- stan("lib/model3_current.stan",  ## this will need to be updated
 #                         data = stan_input_lst,
 #                         warmup = 2000, iter = 3000, chains = 4, thin=4,
 #                         control=list(adapt_delta=0.95))
@@ -351,7 +351,7 @@ stan_train_input_lst <- prepareDataStanTrain(current_cohort)
 stan_train_input_lst$alpha_id <- expert_rank_10[[current_cohort]][1]
 stan_train_input_lst$delta_1 <- rep(0.5,stan_train_input_lst$N_til)
 
-fit_cohort_mmhp <- stan("lib/model3_1_bound.stan",  ## this will need to be updated also
+fit_cohort_mmhp <- stan("lib/model3_current.stan",  ## this will need to be updated also
                         data = stan_train_input_lst,
                         warmup = 1000, iter = 2000, chains = 4,
                         control=list(adapt_delta=0.9))
