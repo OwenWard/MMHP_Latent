8ab95281d3748b3294aa28eae1fe6e60e4a63cac
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2020-10-27 10:28:30 -0400

    update sims from m1 and m2 to correctly use sim events for latent state estimation

--- run_scripts/sim_fits_M2.R
+++ run_scripts/sim_fits_M2.R
@@ -1,6 +1,6 @@
 #### Rerun the modified simulation fits based on the modified Stan models ####
 
-#### Here we simulate data from Model 1 and fit all 3 models to that
+#### Here we simulate data from Model 2 and fit all 3 models to that
 
 
 #### if running on cluster ####
@@ -26,9 +26,11 @@ source('lib/drawIntensity.R')
 n_sim <- 50
 cut_off <- 3
 
-model1_fn <- list(alpha.fun = function(x,y,eta1,eta2,eta3){return(eta1*x*y*exp(-eta2*abs(x-y))/(1+exp(-eta3*(x-y))))})
+model1_fn <- list(alpha.fun = function(x,y,eta1,eta2,eta3){
+  return(eta1*x*y*exp(-eta2*abs(x-y))/(1+exp(-eta3*(x-y))))})
 
-model3_fn <- list(alpha.fun = function(x,y,eta1,eta2){return(eta1*x*y*exp(-eta2*abs(x-y)))},
+model3_fn <- list(alpha.fun = function(x,y,eta1,eta2){
+  return(eta1*x*y*exp(-eta2*abs(x-y)))},
                   q1.fun = function(x,y,eta3){return(exp(-eta3*x))},
                   q0.fun = function(x,y,eta3){return(exp(-eta3*y))})
 
@@ -65,17 +67,18 @@ object_matrix <- list(lambda0_matrix=matrix(object_par$sim_lambda_0,
 # object_matrix$q2_matrix/(object_matrix$q2_matrix+object_matrix$q1_matrix)
 
 ## Simulate
-sim_model1_data <- rep(list(),n_sim)
+sim_model2_data <- rep(list(),n_sim)
 N_array <- array(0,c(n_sim,5,5))
 for(i in c(1:n_sim)){
-  sim_model1_data[i][[1]] <- simulateLatentHP(lambda0_matrix=object_matrix$lambda0_matrix,
-                                                # lambda1_matrix=object_matrix$lambda1_matrix,
-                                                alpha_matrix=object_matrix$alpha_matrix,
-                                                beta_matrix=object_matrix$beta_matrix,
-                                                # q1_matrix=object_matrix$q1_matrix,
-                                                # q2_matrix=object_matrix$q2_matrix,
-                                                horizon=200)
-  clean_sim_data <- cleanSimulationData(raw_data=sim_model1_data[i][[1]], 
+  ### TO DO, update to sim from model 2 with degree correction
+  sim_model2_data[i][[1]] <- simulateLatentHP(lambda0_matrix=object_matrix$lambda0_matrix,
+                                              # lambda1_matrix=object_matrix$lambda1_matrix,
+                                              alpha_matrix=object_matrix$alpha_matrix,
+                                              beta_matrix=object_matrix$beta_matrix,
+                                              # q1_matrix=object_matrix$q1_matrix,
+                                              # q2_matrix=object_matrix$q2_matrix,
+                                              horizon=200)
+  clean_sim_data <- cleanSimulationData(raw_data=sim_model2_data[i][[1]], 
                                         cut_off = cut_off, N = length(object_par$f_vec_1))
   N_array[i,,] <- clean_sim_data$N_count
 }
@@ -84,13 +87,13 @@ for(i in c(1:n_sim)){
 dir.create(data_path, recursive = TRUE, showWarnings = FALSE)
 save(object_fn, object_par, 
      object_matrix, sim_model1_data, 
-     file = paste(data_path,"sim_model1.RData",sep=''))
+     file = paste(data_path,"sim_model2.RData",sep=''))
 
 
 
 ### Fit each model to simulated data ####
 
-load(paste(data_path,"sim_model1.RData",sep=''))
+load(paste(data_path,"sim_model2.RData",sep=''))
 
 ## fit in the model
 sim_model1_stan_fit1 <- rep(list(),n_sim)
@@ -102,7 +105,7 @@ model2 <- stan_model("lib/sim_model2.stan")
 model3 <- stan_model("lib/sim_model3.stan")
 
 for(i in c(1:n_sim)){
-  clean_sim_data <- cleanSimulationData(raw_data=sim_model1_data[i][[1]], 
+  clean_sim_data <- cleanSimulationData(raw_data=sim_model2_data[i][[1]], 
                                         cut_off = cut_off, N = length(object_par$f_vec_1))
   #stan_data <- prepareDataStan(clean_sim_data)
   # these have different stan files because they're simulation models
@@ -121,37 +124,37 @@ for(i in c(1:n_sim)){
                     max_interevent = clean_sim_data$max_interevent)
   print(paste(i,"model1"))
   ## Fit in model 1
-  sim_model1_stan_fit1[i][[1]] <- sampling(model1, data=data_list, 
+  sim_model2_stan_fit1[i][[1]] <- sampling(model1, data=data_list, 
                                            iter=1000, chains=4)
   print("model2")
   ## Fit in model 2
-  sim_model1_stan_fit2[i][[1]] <- sampling(model2,
+  sim_model2_stan_fit2[i][[1]] <- sampling(model2,
                                            data=data_list,
                                            iter=1000, chains=4)
   print("model3")
   ## Fit in model 3
-  sim_model1_stan_fit3[i][[1]] <- sampling(model3,
+  sim_model2_stan_fit3[i][[1]] <- sampling(model3,
                                            data=data_list,
                                            iter=1000, chains=4)
   
 }
 
 ## Extract the model fit
-sim_model1_stan_sim1 <- rep(list(),n_sim)
-sim_model1_stan_sim2 <- rep(list(),n_sim)
-sim_model1_stan_sim3 <- rep(list(),n_sim)
+sim_model2_stan_sim1 <- rep(list(),n_sim)
+sim_model2_stan_sim2 <- rep(list(),n_sim)
+sim_model2_stan_sim3 <- rep(list(),n_sim)
 for(i in 1:n_sim){
-  sim_model1_stan_sim1[[i]] <- rstan::extract(sim_model1_stan_fit1[[i]])
-  sim_model1_stan_sim2[[i]] <- rstan::extract(sim_model1_stan_fit2[[i]])
-  sim_model1_stan_sim3[[i]] <- rstan::extract(sim_model1_stan_fit3[[i]])
+  sim_model2_stan_sim1[[i]] <- rstan::extract(sim_model2_stan_fit1[[i]])
+  sim_model2_stan_sim2[[i]] <- rstan::extract(sim_model2_stan_fit2[[i]])
+  sim_model2_stan_sim3[[i]] <- rstan::extract(sim_model2_stan_fit3[[i]])
 }
 
 ## Save the output
-save(object_fn, object_par, object_matrix, sim_model1_data,
-     sim_model1_stan_fit1, sim_model1_stan_sim1,
-     sim_model1_stan_fit2, sim_model1_stan_sim2,
-     sim_model1_stan_fit3, sim_model1_stan_sim3,
-     file = paste(data_path,"sim_model1_fit123.RData",sep=''))
+save(object_fn, object_par, object_matrix, sim_model2_data,
+     sim_model2_stan_fit1, sim_model2_stan_sim1,
+     sim_model2_stan_fit2, sim_model2_stan_sim2,
+     sim_model2_stan_fit3, sim_model2_stan_sim3,
+     file = paste(data_path,"sim_model2_fit123.RData",sep=''))
 
 
 
@@ -167,24 +170,20 @@ for(cur_sim in c(1:n_sim)){
   interpolate_state_est_lst[[cur_sim]] <- matrix(list(),
                                                  nrow=length(object_par$f_vec_1),
                                                  ncol=length(object_par$f_vec_1))
-  mmhp_par_est <- list(lambda0=mean(sim_model1_stan_sim3[[cur_sim]]$lambda0[1001:2000]),
-                       lambda1=mean(sim_model1_stan_sim3[[cur_sim]]$lambda1[1001:2000]),
-                       eta_1=mean(sim_model1_stan_sim3[[cur_sim]]$eta_1[1001:2000]),
-                       eta_2=mean(sim_model1_stan_sim3[[cur_sim]]$eta_2[1001:2000]),
-                       eta_3=mean(sim_model1_stan_sim3[[cur_sim]]$eta_3[1001:2000]),
-                       beta=mean(sim_model1_stan_sim3[[cur_sim]]$beta[1001:2000]),
-                       f=apply(sim_model1_stan_sim3[[cur_sim]]$f[1001:2000,],2,mean))
-  clean_sim_data <- cleanSimulationData(raw_data=sim_model1_data[cur_sim][[1]], 
+  mmhp_par_est <- list(lambda0=mean(sim_model2_stan_sim3[[cur_sim]]$lambda0[1001:2000]),
+                       lambda1=mean(sim_model2_stan_sim3[[cur_sim]]$lambda1[1001:2000]),
+                       eta_1=mean(sim_model2_stan_sim3[[cur_sim]]$eta_1[1001:2000]),
+                       eta_2=mean(sim_model2_stan_sim3[[cur_sim]]$eta_2[1001:2000]),
+                       eta_3=mean(sim_model2_stan_sim3[[cur_sim]]$eta_3[1001:2000]),
+                       beta=mean(sim_model2_stan_sim3[[cur_sim]]$beta[1001:2000]),
+                       f=apply(sim_model2_stan_sim3[[cur_sim]]$f[1001:2000,],2,mean))
+  clean_sim_data <- cleanSimulationData(raw_data=sim_model2_data[cur_sim][[1]], 
                                         cut_off = 1, N = length(object_par$f_vec_1))
   for(cur_i in c(1:length(object_par$f_vec_1))){
     for(cur_j in c(1:length(object_par$f_vec_1))[-cur_i]){
-      ### this not given for other models
-      # test.mmhp <- sim_model1_data[cur_sim][[1]]$mmhp_matrix[cur_i,cur_j][[1]]
+      # test.mmhp <- sim_model2_data[cur_sim][[1]]$mmhp_matrix[cur_i,cur_j][[1]]
       sim_events <- clean_sim_data$day_hour[which(clean_sim_data$start == cur_i &
                                                     clean_sim_data$end == cur_j)]
-      ### need to do something else with it
-      ### even though just simulated events
-      ### get it from clean_sim_data
       object_hat <- list(lambda0=mmhp_par_est$lambda0,
                          lambda1=mmhp_par_est$lambda1,
                          alpha=model3_fn$alpha.fun(mmhp_par_est$f[cur_i],
@@ -214,4 +213,4 @@ for(cur_sim in c(1:n_sim)){
 }
 
 save(event_state_est_lst,interpolate_state_est_lst,
-     file = paste(data_path,"fit123_state_est_M1.RData",sep=''))
+     file = paste(data_path,"fit123_state_est_M2.RData",sep=''))
