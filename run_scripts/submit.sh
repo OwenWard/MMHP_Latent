ead6fd34f59ea17a1432de0b0c693d7174726d7f
Owen <owengward@gmail.com>
2020-11-24 18:00:19 -0500

    more informative output

--- run_scripts/submit.sh
+++ run_scripts/submit.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 #
 #SBATCH -A tzsts
-#SBATCH -J immhp
-#SBATCH -c 8
+#SBATCH -J cmmhp
+#SBATCH -c 4
 #SBATCH -t 720:00
-#SBATCH --mem-per-cpu 12gb
-#SBATCH -a 1-10
+#SBATCH --mem-per-cpu 8gb
+#SBATCH -a 1,2,3
 #SBATCH --mail-user=ogw2103@columbia.edu
 #SBATCH --mail-type=ALL
+##SBATCH -o cmmhp-%A.%a.out
+##SBATCH --error=cmmhp-%A.%a.err 
 
 module load R
 echo "Launching R"
 date
 
-R CMD BATCH --no-save --vanilla i_mmhp.R routput_immhp
+R CMD BATCH --no-save --vanilla c_mmhp.R routput_cmmhp_$SLURM_ARRAY_TASK_ID
 echo "Completed"
 date
 
