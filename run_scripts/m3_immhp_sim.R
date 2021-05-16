f3bca594ecc830c5125d76604b9f28b8d2f64277
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2021-05-16 14:46:02 -0400

    first just simulate data

--- run_scripts/m3_immhp_sim.R
+++ run_scripts/m3_immhp_sim.R
@@ -6,11 +6,12 @@ source("/rigel/stats/users/ogw2103/code/MMHP/MMHP_Latent/run_scripts/cluster_set
 
 data_path <- "output/revisions/"
 
-library(rstan)
+# library(rstan)
+library(cmdstanr)
 library(R.utils)
 # library(compete)
 options(mc.cores = parallel::detectCores())
-rstan_options(auto_write = TRUE)
+# rstan_options(auto_write = TRUE)
 jobid <- Sys.getenv("SLURM_ARRAY_TASK_ID")
 jobid <- as.numeric(jobid)
 sim_id <- jobid
@@ -105,231 +106,104 @@ N_array <- clean_sim_data$N_count
 dir.create(data_path, recursive = TRUE, showWarnings = FALSE)
 save(object_fn, object_par, 
      object_matrix, sim_model3_data, 
-     file = paste(data_path,"sim_model3_",sim_id,".RData",sep=''))
+     file = paste(data_path,"sim_model3_immhp_", ".RData", sep=''))
 
 
-
+##### comment out above for fitting ####
 ### Fit each model to simulated data ####
 
-load(paste(data_path, "sim_model3_", sim_id, ".RData", sep = ''))
-
-## fit in the model
-# sim_model3_stan_fit1 <- list()
-# sim_model3_stan_fit2 <- list()
-sim_model_immhp_stan_fit <- list()
-
-immhp_model <- stan_model("lib/sim_immhp.stan")
-# model1 <- stan_model("lib/sim_model1.stan")
-# model2 <- stan_model("lib/sim_model2.stan")
-# model3 <- stan_model("lib/sim_model3_dc.stan")
-
-# for(i in c(1:n_sim)){
-i <- sim_id
-clean_sim_data <- cleanSimulationData(raw_data=sim_model3_data, 
-                                      cut_off = cut_off,
-                                      N = length(object_par$f_vec_1))
-#stan_data <- prepareDataStan(clean_sim_data)
-# these have different stan files because they're simulation models
-
-
-data_list <- list(max_Nm = max(clean_sim_data$N_count),
-                  N_til = length(clean_sim_data$I_fit),
-                  M=sum(clean_sim_data$N_count>=cut_off),
-                  N=length(object_par$f_vec_1),
-                  I_fit = clean_sim_data$I_fit,
-                  J_fit = clean_sim_data$J_fit,
-                  T = tail(clean_sim_data$day_hour,1),
-                  Nm = as.vector(clean_sim_data$N_count[clean_sim_data$N_count>=cut_off]),
-                  event_matrix = clean_sim_data$event_matrix,
-                  interevent_time_matrix = clean_sim_data$time_matrix,
-                  max_interevent = clean_sim_data$max_interevent)
-# print(paste(i,"model1"))
-## Fit in model 1
-# start_time <- Sys.time()
-# sim_model3_stan_fit1[[1]] <- sampling(model1, data=data_list, 
-#                                          iter=1000, chains=4)
-# m1_time <- Sys.time() - start_time
-
-# print("model2")
-# ## Fit in model 2
-# start_time <- Sys.time()
-# sim_model3_stan_fit2[[1]] <- sampling(model2,
-#                                          data=data_list,
-#                                          iter=1000, chains=4)
-# m2_time <- Sys.time() - start_time
-
-# print("model3")
+# load(paste(data_path, "sim_model3_immhp_", ".RData", sep = ''))
+# 
+# 
+# sim_model_immhp_stan_fit <- list()
+# time_vec <- clean_sim_data$event_matrix[sim_id, ]
+# time_vec <- time_vec[time_vec > 0]
+# diff_times <- diff(c(0, time_vec, obs_time))
+# a = as.matrix(diff_times, nrow = 1, ncol = length(diff_times))
+# 
+# immhp_model <- cmdstan_model("lib/mmhp_single.stan")
+# 
+# 
+# print("I-mmhp")
 # ## Fit in model 3
 # start_time <- Sys.time()
-# sim_model3_stan_fit3[[1]] <- sampling(model3,
-#                                       data=data_list,
-#                                       iter=1000, chains=4,
-#                                       control = list(adapt_delta = 0.95,
-#                                                      max_treedepth = 15))
-# m3_time <- Sys.time() - start_time
-print("I-mmhp")
-## Fit in model 3
-start_time <- Sys.time()
-sim_model_immhp_stan_fit[[1]] <- sampling(immhp_model,
-                                      data=data_list,
-                                      iter=1000, chains=4,
-                                      control = list(adapt_delta = 0.95,
-                                                     max_treedepth = 15))
-immhp_time <- Sys.time() - start_time
-
-
-# }
-
-## Extract the model fit
-# sim_model3_stan_sim1 <- list()
-# sim_model3_stan_sim2 <- list()
-sim_model_stan_sim_immhp <- list()
-
-# sim_model3_stan_sim1 <- rstan::extract(sim_model3_stan_fit1[[1]])
-# sim_model3_stan_sim2 <- rstan::extract(sim_model3_stan_fit2[[1]])
-sim_model_stan_sim_immhp <- rstan::extract(sim_model_immhp_stan_fit[[1]])
-
-# m1_rank <- order(apply(sim_model3_stan_sim1$f, 2, mean))
-# m2_rank <- order(apply(sim_model3_stan_sim2$f, 2, mean))
-# m3_rank <- order(apply(sim_model3_stan_sim3$f, 2, mean))
-
-## Save the output
-save(object_fn, object_par, object_matrix, sim_model3_data,
-     # sim_model3_stan_fit1, sim_model3_stan_sim1,
-     # sim_model3_stan_fit2, sim_model3_stan_sim2,
-     sim_model_immhp_stan_fit, sim_model_stan_sim3_immhp,
-     file = paste(data_path,"sim_model3_fit_immhp_",
-                  sim_id,
-                  ".RData",sep=''))
-
-### not sure if stuff below here will work
-
-
-#### Interpolate the Latent States ####
-
-event_state_est_lst <- list()
-interpolate_state_est_lst <- list()
-# for(cur_sim in c(1:n_sim)){
-#   print(cur_sim)
-event_state_est_lst <- matrix(list(),
-                              nrow = length(object_par$f_vec_1),
-                              ncol = length(object_par$f_vec_1))
-interpolate_state_est_lst <- matrix(list(),
-                                    nrow = length(object_par$f_vec_1),
-                                    ncol = length(object_par$f_vec_1))
-
-
-
-lambda_0_est <- apply(sim_model3_stan_sim3$lambda0, 2, mean)
-lambda_1_est <- apply(sim_model3_stan_sim3$lambda1, 2, mean)
-## then put these into a matrix
-lam0_par_est <- matrix(0, nrow = length(object_par$f_vec_1),
-                       ncol = length(object_par$f_vec_1))
-lam1_par_est <- matrix(0, nrow = length(object_par$f_vec_1),
-                       ncol = length(object_par$f_vec_1))
-for(i in seq_along(lambda_0_est)) {
-  row_id <- clean_sim_data$I_fit[i]
-  col_id <- clean_sim_data$J_fit[i]
-  lam0_par_est[row_id, col_id] <- lambda_0_est[i]
-  lam1_par_est[row_id, col_id] <- lambda_1_est[i]
-}
-
-
-mmhp_par_est <- list(lambda0 = lam0_par_est,
-                     lambda1 = lam1_par_est,
-                     eta_1 = mean(sim_model3_stan_sim3$eta_1),
-                     eta_2 = mean(sim_model3_stan_sim3$eta_2),
-                     eta_3 = mean(sim_model3_stan_sim3$eta_3),
-                     beta = mean(sim_model3_stan_sim3$beta),
-                     f = apply(sim_model3_stan_sim3$f, 2, mean))
-clean_sim_data <- cleanSimulationData(raw_data=sim_model3_data, 
-                                      cut_off = 1, N = length(object_par$f_vec_1))
-for(cur_i in c(1:length(object_par$f_vec_1))){
-  for(cur_j in c(1:length(object_par$f_vec_1))[-cur_i]){
-    test.mmhp <- sim_model3_data$mmhp_matrix[cur_i,cur_j][[1]]
-    object_hat <- list(lambda0=mmhp_par_est$lambda0[cur_i, cur_j],
-                       lambda1=mmhp_par_est$lambda1[cur_i, cur_j],
-                       alpha=model3_fn$alpha.fun(mmhp_par_est$f[cur_i],
-                                                 mmhp_par_est$f[cur_j],
-                                                 mmhp_par_est$eta_1,
-                                                 mmhp_par_est$eta_2),
-                       beta=mmhp_par_est$beta,
-                       q1=model3_fn$q1.fun(mmhp_par_est$f[cur_i],
-                                           mmhp_par_est$f[cur_j],
-                                           mmhp_par_est$eta_3),
-                       q2=model3_fn$q0.fun(mmhp_par_est$f[cur_i],
-                                           mmhp_par_est$f[cur_j],
-                                           mmhp_par_est$eta_3))
-    viterbi_result <- myViterbi(events = test.mmhp$tau[-1], 
-                                param = object_hat,
-                                termination = obs_time)
-    latent_inter <- interpolateLatentTrajectory(object_hat, 
-                                                test.mmhp$tau[-1], 
-                                                viterbi_result$zt_v,
-                                                initial.state = viterbi_result$initial_state,
-                                                termination.time = obs_time,
-                                                termination.state = viterbi_result$termination_state)
-    event_state_est_lst[cur_i,cur_j][[1]] <- viterbi_result
-    interpolate_state_est_lst[cur_i,cur_j][[1]] <- latent_inter
-  }
-}
-# }
-
-save(event_state_est_lst, interpolate_state_est_lst,
-     file = paste(data_path, "fit123_state_est_", sim_id, ".RData", sep=''))
-
-
-cat("Model 1 Time:", m1_time, "\n")
-cat("Model 2 Time:", m2_time, "\n")
-cat("Model 3 Time:", m3_time, "\n")
-
-
-#### Get Other Rankings
-
-# count_data_dc <- get_wl_matrix(df = cbind(clean_sim_data$start, 
-#                                           clean_sim_data$end))
-# isi_dc.out <- compete::isi98(m = count_data_dc, random = TRUE)
-# isi_rank_dc <- as.numeric(rev(isi_dc.out$best_order))
+# sim_model_immhp_stan_fit <- immhp_model$sample(data = list(N_til = 1,
+#                                                 max_Nm = length(time_vec),
+#                                                 Nm = as.array(length(time_vec)),
+#                                                 time_matrix= t(a),
+#                                                 max_interevent =
+#                                                 as.array(max(diff_times))),
+#                                            chains = 4, thin = 5,
+#                                            iter_sampling = 2500,
+#                                            refresh = 500,
+#                                            adapt_delta = 0.9)
+# immhp_time <- Sys.time() - start_time
+# sim_model_immhp_stan_fit$summary()
 # 
 # 
-# agg_rank_data <- clean_sim_data_dc$N_count
-# agg_rank_model <- stan_model(here("lib","latent_rank_agg_sim.stan"))
+# sim_model_stan_sim_immhp <- sim_model_immhp_stan_fit$draws()
+# post_draws <- posterior::as_draws_df(sim_model_stan_sim_immhp)
 # 
-# agg_rank_fit <- rstan::sampling(agg_rank_model,
-#                                 data = list(n = 5,
-#                                             n_matrix = agg_rank_data),
-#                                 iter = 1000,
-#                                 chains = 4)
-# agg_sims <- rstan::extract(agg_rank_fit)
+# # }
 # 
-# agg_rank <- order(apply(agg_sims$x, 2, mean))
+# ### save true parameters also
+# i_ind <- clean_sim_data$I_fit[sim_id]
+# j_ind <- clean_sim_data$J_fit[sim_id]
+# 
+# true_mmhp <- list(lambda1 = object_matrix$lambda1_matrix[i_ind, j_ind],
+#                   lambda0 = object_matrix$lambda0_matrix[i_ind, j_ind],
+#                   alpha = object_matrix$alpha_matrix[i_ind, j_ind],
+#                   beta = object_matrix$beta_matrix[i_ind, j_ind],
+#                   q1 = object_matrix$q1_matrix[i_ind, j_ind],
+#                   q2 = object_matrix$q2_matrix[i_ind, j_ind])
+# 
+# mean_fit <- list(lambda1 = mean(post_draws$`lambda1[1]`),
+#                  lambda0 = mean(post_draws$`lambda0[1]`),
+#                  alpha = mean(post_draws$`alpha[1]`),
+#                  beta = mean(post_draws$beta),
+#                  q1 = mean(post_draws$`q1[1]`),
+#                  q2 = mean(post_draws$`q2[1]`))
+# 
+# ## Save the output
+# save(true_mmhp, mean_fit,
+#      sim_model_immhp_stan_fit, sim_model_stan_sim3_immhp,
+#      file = paste(data_path,"sim_model3_fit_immhp_",
+#                   sim_id,
+#                   ".RData",sep=''))
+# 
+# ### not sure if stuff below here will work
+# 
+# 
+# #### Interpolate the Latent States ####
+# 
+# event_state_est_lst <- list()
+# interpolate_state_est_lst <- list()
+# 
+# mmhp_par_est <- mean_fit
+# 
+# # delta_vec <- c(mean(sim_model_stan_sim_immhp$delta_1), 
+# #                1- mean(sim_model_stan_sim_immhp$delta_1))
 # 
 # 
 # 
-# ### glicko ranking also ####
-# glicko_data <- tibble(start = clean_sim_data$start,
-#                       end = clean_sim_data$end)
+# viterbi_result <- myViterbi(events = time_vec,
+#                             param = mmhp_par_est,
+#                             termination = obs_time)
+# latent_inter <- interpolateLatentTrajectory(mmhp_par_est,
+#                                             time_vec,
+#                                             viterbi_result$zt_v,
+#                                             initial.state = 
+#                                               viterbi_result$initial_state,
+#                                             termination.time = obs_time,
+#                                             termination.state = 
+#                                               viterbi_result$termination_state)
+# event_state_est_lst <- viterbi_result
+# interpolate_state_est_lst <- latent_inter
 # 
-# glicko_data <- glicko_data %>%
-#   mutate(id = row_number(), score = 1) %>%
-#   select(id, start, end, score)
 # 
-# gl_train <- my_glicko(glicko_data, history=TRUE, cval=2)
+# save(event_state_est_lst, interpolate_state_est_lst,
+#      file = paste(data_path,
+#                   "indep_mmhp_state_est_",
+#                   sim_id,
+#                   ".RData", sep=''))
 # 
-# gl_train
-# 
-# gl_ranks <- order(gl_train$ratings$Rating)
-# 
-# ## then save these
-# 
-# output_rank <- tibble(truth = 1:5, 
-#                       m1 = m1_rank,
-#                       m2 = m2_rank,
-#                       m3_dc = m3_dc_rank,
-#                       isi = isi_rank_dc,
-#                       agg = agg_rank,
-#                       glicko = gl_ranks,
-#                       sim = rep(sim_id,5))
-# 
-# saveRDS(output_rank, 
-#         file = paste(data_path,"rank_sim",sim_id,".RDS",sep=''))
