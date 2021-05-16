f448cd79800815909e484c08298aa329deecca73
Owen W <owengward@gmail.com>
2021-05-16 14:52:16 -0400

    update for moto

--- run_scripts/cluster_setup.R
+++ run_scripts/cluster_setup.R
@@ -2,6 +2,6 @@
 
 
 .libPaths("/moto/stats/users/ogw2103/rpackages")
-setwd("/moto/stats/users/ogw2103/code/MMHP/MMHP_Latent/")
+setwd("/moto/stats/users/ogw2103/Code/MMHP_Latent/")
 library(here)
 library(R.utils)
