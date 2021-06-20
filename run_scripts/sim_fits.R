786f2a28c2a87a9feb514df429adb543926251dd
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2021-06-20 14:57:22 -0400

    match real data param values a bit better

--- run_scripts/sim_fits.R
+++ run_scripts/sim_fits.R
@@ -4,7 +4,7 @@
 #### if running on cluster ####
 source("/moto/stats/users/ogw2103/Code/MMHP_Latent/run_scripts/cluster_setup.R")
 
-data_path <- "output/revisions/sim_m3/june_18/"
+data_path <- "output/revisions/sim_m3/june_20/"
 
 library(cmdstanr)
 library(R.utils)
@@ -51,13 +51,13 @@ object_fn <- list(alpha.fun = function(x,y,eta1,eta2){
                   q0.fun = function(x,y,eta3){return(exp(-eta3*y))})
 
 object_par <- list(sim_lambda_1 = 0.5,
-                   sim_eta_1 = 2.5,
                    gamma_var = seq(from = 0.01,
                                    to = 0.2,
                                    length.out = num_nodes),
                    zeta_var = rep(0.05, num_nodes),
+                   sim_eta_1 = 3.5,
                    sim_eta_2 = 0.6,
-                   sim_eta_3 = 7.5,
+                   sim_eta_3 = 3.0,
                    sim_beta = 2,
                    f_vec_1 = seq(from = 0.05, to = 0.95,
                                  length.out = num_nodes))
