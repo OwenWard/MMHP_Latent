d969e47158b470c65e4753c7fe35f965d8898192
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2021-06-28 16:53:08 -0400

    modify sim fits again

--- run_scripts/sim_fits.R
+++ run_scripts/sim_fits.R
@@ -4,7 +4,7 @@
 #### if running on cluster ####
 source("/moto/stats/users/ogw2103/Code/MMHP_Latent/run_scripts/cluster_setup.R")
 
-data_path <- "output/revisions/sim_m3/june_18/"
+data_path <- "output/revisions/sim_m3/june_22/"
 
 library(cmdstanr)
 library(R.utils)
@@ -50,18 +50,16 @@ object_fn <- list(alpha.fun = function(x,y,eta1,eta2){
                   q1.fun = function(x,y,eta3){return(exp(-eta3*x))},
                   q0.fun = function(x,y,eta3){return(exp(-eta3*y))})
 
-object_par <- list(sim_lambda_1 = 0.4,
-                   gamma_var = seq(from = 0.01,
-                                    to = .2,
+object_par <- list(sim_lambda_1 = 0.6,
+                   gamma_var = seq(from = 0.01, to = 0.2,
                                    length.out = num_nodes),
-                   zeta_var = rep(0.1, num_nodes),
+                   zeta_var = rep(0.05, num_nodes),
                    sim_eta_1 = 1, # from 3.5
-                   sim_eta_2 = 2, # from 2.6
+                   sim_eta_2 = 2,#1.5, # from 2.6
                    sim_eta_3 = 3, # this seems better
                    #sim_eta_3 = 7.5,
                    sim_beta = 1.5, # from 2
-                   f_vec_1 = seq(from = 0.10,
-                                 to = 0.9,
+                   f_vec_1 = seq(from = 0.2, to = 0.9,
                                  length.out = num_nodes))
 
 
