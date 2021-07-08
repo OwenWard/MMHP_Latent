fb673f8ef86003e7ad71cb052969ceef34f6da11
Owen Ward <33092609+OwenWard@users.noreply.github.com>
2021-07-08 11:13:49 -0400

    modify the simulation ever so slightly again

--- run_scripts/sim_fits.R
+++ run_scripts/sim_fits.R
@@ -4,7 +4,7 @@
 #### if running on cluster ####
 source("/moto/stats/users/ogw2103/Code/MMHP_Latent/run_scripts/cluster_setup.R")
 
-data_path <- "output/revisions/sim_m3/sim_2/"
+data_path <- "output/revisions/sim_m3/sim_4/"
 
 library(cmdstanr)
 library(R.utils)
@@ -32,7 +32,7 @@ source('lib/drawIntensity.R')
 n_sim <- 1
 num_nodes <- 10
 cut_off <- 3
-obs_time <- 100
+obs_time <- 200
 
 model1_fn <- list(alpha.fun = function(x, y, eta1, eta2, eta3){
   return(eta1 * x * y * exp(-eta2 * abs(x-y))/(1 + exp(-eta3 *(x-y))))})
@@ -56,7 +56,7 @@ object_par <- list(sim_lambda_1 = 0.6,
                    zeta_var = rep(0.1, num_nodes),
                    sim_eta_1 = 1, # from 3.5
                    sim_eta_2 = 2,#1.5, # from 2.6
-                   sim_eta_3 = 1.5, # this seems better
+                   sim_eta_3 = 1.25, # this seems better
                    #sim_eta_3 = 7.5,
                    sim_beta = 1.5, # from 2
                    f_vec_1 = seq(from = 0.2, to = 0.9,
